ad5e8d46900640cbca0051d181f5c4ef359cfc99
MusicJunkyREZ <reidzuckerman@gmail.com>
2022-03-12 00:02:27 -0500

    sendgrid updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
+const sendgrindTransport = require('nodemailer-sendgrid-transport');
 require('dotenv').config();
 
 console.log(process.env);
@@ -13,9 +14,17 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
 app.use(express.json());
 
+const transport = nodemailer.createTransport(sendgrindTransport({
+    auth:{
+        api_key: api_key_code
+    }
+}))
+
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + `/public/index.html`)
 });
+
+
 
 // Code from EmailJS
 
@@ -25,6 +34,16 @@ app.get('/', (req, res)=>{
 // After refreshing the page with the Gmail that is logged in, it should work.
 app.post('/', (req, res) => {
     console.log(req.body);
+
+    transport.sendMail({
+        to: `reidzuckerman@gmail.com`,
+        from: `reidzuckermanmusic@gmail.com`,
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        html:`<h1>Message from ${req.body.email}: ${req.body.subject}</h1>
+            <br>
+            <h2>${req.body.message}</h2>`
+    })
+    .catch(err => console.log(err));
 
     // const transporter = nodemailer.createTransport({
     //     service: 'gmail',
@@ -37,32 +56,32 @@ app.post('/', (req, res) => {
     //  set the new transporter service to this from this article https://jay315.medium.com/sending-email-using-express-js-with-nodemailer-in-heroku-71741f29463c
     // seemed like the UNLOCK CAPTCHA page only worked temporarily, so had to allow POP and SMTP from everywhere from this article https://support.google.com/mail/thread/9828582/how-to-unlock-captcha?hl=en
     // person here says port 465 needs to be open on heroku https://stackoverflow.com/questions/44729580/how-can-i-get-nodemailer-to-work-with-heroku
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'reidzuckermanmusic@gmail.com',
-            pass: `${password}`
-        }
-    })
+    // const transporter = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         user: 'reidzuckermanmusic@gmail.com',
+    //         pass: `${password}`
+    //     }
+    // })
 
-    const mailOptions = {
-        from: req.body.email,
-        to: 'reidzuckermanmusic@gmail.com',
-        subject: `Message from ${req.body.email}: ${req.body.subject}`,
-        text: req.body.message
-    }
+    // const mailOptions = {
+    //     from: req.body.email,
+    //     to: 'reidzuckermanmusic@gmail.com',
+    //     subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    //     text: req.body.message
+    // }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if(error){
-            console.log(error);
-            res.send('error');
-        } else {
-            console.log(`Email sent: ` + info.response);
-            res.send('success');
-        }
-    })
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if(error){
+    //         console.log(error);
+    //         res.send('error');
+    //     } else {
+    //         console.log(`Email sent: ` + info.response);
+    //         res.send('success');
+    //     }
+    // })
 })
 
 // OUTLOOK
